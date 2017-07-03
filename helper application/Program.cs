b26cedabf636ee7a372a956179796b2136b5e61b
rikumax25 <jorgev259@outlook.es>
2017-07-03 15:46:10 -0600

    initial push

---
+++ helper application/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace helper_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = Environment.CurrentDirectory + "/sav.dat";
+            var filename2 = Environment.CurrentDirectory + "/pokeprism.sav";
+
+            var dat = hexArray(filename);
+            var sav = hexArray(filename2);
+
+            for(int i = 0; i <= hexToInt("7FF0"); i++)
+            {
+                dat[i] = sav[i];
+            }
+
+            File.WriteAllBytes("conv.dat", GetBytes(dat));
+        }
+
+        static string[] hexArray(string path)
+        {
+            byte[] data = File.ReadAllBytes(path);
+            string hex = BitConverter.ToString(data);
+            return hex.Split("-".ToCharArray()[0]);
+        }
+
+        static byte[] GetBytes(string[] strArray)
+        {
+            var str = String.Join("-", strArray);
+
+           var array = str.Split('-').Select(b => Convert.ToByte(b, 16)).ToArray();
+
+            return array;
+        }
+
+        static int hexToInt(string hexValue)
+        {
+            return int.Parse(hexValue, System.Globalization.NumberStyles.HexNumber);
+        }
+    }
+}
